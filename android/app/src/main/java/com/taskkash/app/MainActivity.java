f25a58cecb8e7890c3eee2d32885c3c7336a88ef
Admin <admin@taskkash.com>
2026-03-09 03:27:49 +0200

    build(android): integrate capacitor for native android application generation

---
+++ android/app/src/main/java/com/taskkash/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.taskkash.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.taskkash.app',
+  appName: 'TaskKash',
+  webDir: 'dist/public'
+};
+
+export default config;
